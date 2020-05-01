b8f75c6f65dfae8e1bd37908037625df70b45353
DhavalKotak <55011445+DhavalKotak@users.noreply.github.com>
2020-05-01 16:38:02 +0530

    Score system added
    Some bugs are still there that are messing with the scoring system

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var height = 600;
-var width = 1000;
+var mposX;
+var mposY;
+var targetX;
+var targetY;
+var targetRadius;
+var distance;
+var score = 0;
 
 function target() {
-  ctx.clearRect(0, 0, width, height);
-  var targetX = Math.floor((Math.random() * 800) + 20);
-  var targetY = Math.floor((Math.random() * 500) + 20);
-  var targetRadius = Math.floor((Math.random() * 20) + 7);
+  ctx.clearRect(0, 0, 1000, 600);
+  targetX = Math.floor((Math.random() * 800) + 20);
+  targetY = Math.floor((Math.random() * 500) + 20);
+  targetRadius = Math.floor((Math.random() * 20) + 8);
   ctx.fillStyle = "green";
   ctx.beginPath();
-  ctx.arc(targetX,targetY,targetRadius,0,2* Math.PI);
+  ctx.arc(targetX,targetY,targetRadius,0,2 * Math.PI);
   ctx.closePath();
   ctx.fill();
-  setTimeout(target ,900);
+
+  function mousePosition(event) {
+     mposX = event.clientX ;
+     mposY = event.clientY ;
+     detectHits();
+  }
+
+  function detectHits() {
+    distance = Math.round(Math.sqrt(Math.pow((targetX - mposX), 2) + Math.pow((targetY - mposY), 2)));
+    if (distance <= targetRadius) {
+      /*if (distance < 2) {
+        score = score + 5;
+      }else {
+        score = score + 1;
+      }*/
+      score++;
+      document.getElementById('score').innerHTML = "Score: " + score;
+    }
+  }
+
+  document.addEventListener("click", mousePosition);
+
+  setTimeout(target ,2000);
 }
+
 target();
