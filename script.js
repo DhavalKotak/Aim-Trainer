207ff62cda4740060cd91a69dac0a50547b08494
DhavalKotak <55011445+DhavalKotak@users.noreply.github.com>
2020-05-01 16:53:02 +0530

    Fixed the scoring system

--- script.js
+++ script.js
@@ -8,6 +8,27 @@ var targetRadius;
 var distance;
 var score = 0;
 
+document.addEventListener("click", mousePosition);
+
+function mousePosition(event) {
+   mposX = event.clientX ;
+   mposY = event.clientY ;
+   detectHits();
+}
+
+function detectHits() {
+  distance = Math.round(Math.sqrt(Math.pow((targetX - mposX), 2) + Math.pow((targetY - mposY), 2)));
+  if (distance <= targetRadius) {
+    if (distance < 2) {
+      score = score + 10;
+    }else {
+      score = score + 5;
+    }
+  }
+    document.getElementById('score').innerHTML = "Score: " + score;
+}
+
+
 function target() {
   ctx.clearRect(0, 0, 1000, 600);
   targetX = Math.floor((Math.random() * 800) + 20);
@@ -18,27 +39,6 @@ function target() {
   ctx.arc(targetX,targetY,targetRadius,0,2 * Math.PI);
   ctx.closePath();
   ctx.fill();
-
-  function mousePosition(event) {
-     mposX = event.clientX ;
-     mposY = event.clientY ;
-     detectHits();
-  }
-
-  function detectHits() {
-    distance = Math.round(Math.sqrt(Math.pow((targetX - mposX), 2) + Math.pow((targetY - mposY), 2)));
-    if (distance <= targetRadius) {
-      /*if (distance < 2) {
-        score = score + 5;
-      }else {
-        score = score + 1;
-      }*/
-      score++;
-      document.getElementById('score').innerHTML = "Score: " + score;
-    }
-  }
-
-  document.addEventListener("click", mousePosition);
 
   setTimeout(target ,2000);
 }
